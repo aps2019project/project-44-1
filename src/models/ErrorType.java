66baab0e7353a16a65452c5915aacda7f84ab287
alrz1999 <alrz1999@gmail.com>
2019-04-14 00:53:00 +0430

    tests of Enum ErrorType added

--- src/models/ErrorType.java
+++ src/models/ErrorType.java
@@ -1,4 +1,18 @@
 package models;
 
 public enum ErrorType {
+    // TODO: 14/04/2019 these are tests
+    INVALID_USERNAME("invalid username"),
+    INVALID_PASSWORD("invalid password");
+
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    ErrorType(String message) {
+        this.message = message;
+    }
 }
