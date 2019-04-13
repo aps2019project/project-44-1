943bbe99f0a766ac7c11db7208704c77535b4eac
alrz1999 <alrz1999@gmail.com>
2019-04-14 00:59:29 +0430

    basics of class CaptureFlag1 added and it inherits Battle

--- src/models/CaptureFlag1.java
+++ src/models/CaptureFlag1.java
@@ -1,4 +1,7 @@
 package models;
 
-public class CaptureFlag1 {
+public class CaptureFlag1 extends Battle {
+    public CaptureFlag1(BattleKind battleKind, Account player1, Account player2) {
+        super(battleKind, player1, player2);
+    }
 }
