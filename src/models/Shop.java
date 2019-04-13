1a0795b2d2ea1a4be83043bde36d29eb931cc43e
alrz1999 <alrz1999@gmail.com>
2019-04-14 00:50:28 +0430

    basics of class Shop added

--- src/models/Shop.java
+++ src/models/Shop.java
@@ -1,4 +1,26 @@
 package models;
 
 public class Shop {
+    // TODO: 11/04/2019 after defining items and cards
+
+
+    public void help() {
+
+    }
+
+    public void showShopCards() {
+
+    }
+
+    public void sell(int cardID) {
+
+    }
+
+    public void buy(String cardName) {
+
+    }
+
+    public void searchInShop(String cardName) {
+
+    }
 }
