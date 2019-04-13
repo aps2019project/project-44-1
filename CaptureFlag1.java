a6dc708ddfc09316eb9122b87e5ec9409e67e068
AhmadZafarani <45295720+AhmadZafarani@users.noreply.github.com>
2019-04-13 18:05:22 +0430

    Delete CaptureFlag1.java

--- CaptureFlag1.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class CaptureFlag1 extends Battle {
-    public CaptureFlag1(BattleKind battleKind, Account player1, Account player2) {
-        super(battleKind, player1, player2);
-    }
-}
