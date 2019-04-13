0994e3f828a35e962f0c0a2b9f8c737e52d5c4d0
AhmadZafarani <45295720+AhmadZafarani@users.noreply.github.com>
2019-04-13 18:10:46 +0430

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import controller.GameController;
-
-public class Main {
-    public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.main();
-    }
-}
