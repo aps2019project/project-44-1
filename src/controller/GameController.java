b04d44e4df21460cd047866690d6125453bd86ce
alrz1999 <alrz1999@gmail.com>
2019-04-14 00:47:31 +0430

    basics of class GameController added

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,5 +1,53 @@
 package controller;
 
-public class GameController {
+import models.Game;
+import view.GameRequest;
+import view.GameView;
 
+public class GameController extends Controller {
+    private Game game = new Game();
+    private GameView view = new GameView();
+
+    @Override
+    public void main() {
+        boolean isFinish = false;
+        do {
+            GameRequest request = new GameRequest();
+            request.getNewCommand();
+            if (request.getType().equals("sign in")) {
+                login(view,request);
+            }
+            if (request.getType().equals("sign up")){
+                createAccount(view,request);
+            }
+            if (request.getType().equals("help")){
+                help(view);
+            }
+            if (request.getType().equals("show leaderboard")){
+                showLeaderboard(view);
+            }
+            if (request.getType().equals("exit")){
+                isFinish = true;
+            }
+
+        }
+        while (!isFinish);
+    }
+
+    private void login(GameView view,GameRequest request) {
+        AccountController accountController = new AccountController();
+        accountController.main(Game.getAccount("username"));
+    }
+
+    private void createAccount(GameView view,GameRequest request) {
+
+    }
+
+    private void showLeaderboard(GameView view){
+
+    }
+
+    private void help(GameView view){
+
+    }
 }
