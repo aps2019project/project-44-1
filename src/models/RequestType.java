eea61e56af310e184809bc1bbc766a9d8afc133c
alrz1999 <alrz1999@gmail.com>
2019-04-14 01:13:13 +0430

    some requests added to RequestType

--- src/models/RequestType.java
+++ src/models/RequestType.java
@@ -1,4 +1,14 @@
 package models;
 
 public enum RequestType {
+    EXIT,
+    HELP,
+    SAVE,
+    LOGIN,
+    LOGOUT,
+    SIGN_UP,
+    SHOW_LEADERBOARD,
+    ENTER_BATTLE,
+    ENTER_SHOP,
+    ENTER_COLLECTION,
 }
